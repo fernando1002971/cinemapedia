c20f748bd61ebb7a518a393d4888be70099a5b03
fernando1002971 <fernando1002971@gmail.com>
2023-08-19 18:58:53 -0600

    Implementación de shellRoute (Navigation Bar) con go_router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,21 +1,38 @@
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
   initialLocation: "/", 
-  routes: [ 
-  GoRoute(
-      path: "/",
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen()
-    ),
-  GoRoute(
-      path: "/movie/:id",
-      name: MovieScreen.name,
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? "no-id";
-        return MovieScreen(movieId: movieId);
-      }
-    ),
- ]
+  routes: [
+
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(childView: child);
+      },
+      routes: [
+        GoRoute(
+            path: "/",
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                  path: "movie/:id",
+                  name: MovieScreen.name,
+                  builder: (context, state) {
+                    final movieId = state.pathParameters['id'] ?? "no-id";
+                    return MovieScreen(movieId: movieId);
+                  }),
+            ]),
+        GoRoute(
+          path: "/favorites",
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        )
+      ])
+]
+
+
 );
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,103 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:cinemapedia/presentation/providers/providers.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = "home-screen";
+  final Widget childView;
 
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(topratedMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-  }
+  const HomeScreen({super.key, required this.childView});
 
   @override
   Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topratedMovies = ref.watch(topratedMoviesProvider);
-
-
-    return Visibility(
-      visible: !initialLoading,
-      replacement: const FullScreenLoader(),
-      child: CustomScrollView(slivers: [
-        const SliverAppBar(
-          floating: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppbar(),
-          ),
-        ),
-        SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) {
-          return Column(
-            children: [
-              //Primer carrusel usando PageView
-              MoviesSlideShow(movies: slideShowMovies),
-    
-              MovieHorizontalListview(
-                movies: nowPlayingMovies,
-                title: "En cines",
-                subTitle: "Lunes 20",
-                loadNextPage: () =>
-                    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage(),
-              ),
-    
-              MovieHorizontalListview(
-                movies: popularMovies,
-                title: "Populares",
-                loadNextPage: () =>
-                    ref.read(popularMoviesProvider.notifier).loadNextPage(),
-              ),
-    
-              MovieHorizontalListview(
-                movies: upcomingMovies,
-                title: "Próximamente",
-                subTitle: "En este mes",
-                loadNextPage: () =>
-                    ref.read(upcomingMoviesProvider.notifier).loadNextPage(),
-              ),
-    
-              MovieHorizontalListview(
-                movies: topratedMovies,
-                title: "Mejor calificadas",
-                subTitle: "Desde siempre",
-                loadNextPage: () =>
-                    ref.read(topratedMoviesProvider.notifier).loadNextPage(),
-              ),
-              const SizedBox(
-                height: 10,
-              )
-            ],
-          );
-        }, childCount: 1))
-      ]),
+    return  Scaffold(
+      body: childView,
+      bottomNavigationBar:const CustomBottomNavigation(),
     );
   }
 }
